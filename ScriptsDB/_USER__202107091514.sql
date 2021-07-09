016d437662a290d0b142d816ac404ec8beafa833
placido.papotto <placido.papotto.ext@leonardocompany.com>
2021-07-09 19:48:22 +0200

    Added new DB scripts

--- ScriptsDB/_USER__202107091514.sql
+++ ScriptsDB/_USER__202107091514.sql
@@ -1,2 +1,17 @@
-INSERT INTO "USER" ("ID","EMAIL","USERNAME","NAME","SURNAME") VALUES
+-- public."USER" definition
+
+-- Drop table
+
+-- DROP TABLE "USER";
+
+CREATE TABLE "USER" (
+	"ID" int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"EMAIL" varchar(125) NOT NULL,
+	"USERNAME" varchar(50) NOT NULL,
+	"NAME" varchar(50) NOT NULL,
+	"SURNAME" varchar(50) NOT NULL,
+	CONSTRAINT "USER_pkey" PRIMARY KEY ("ID")
+);
+
+INSERT INTO "USER" ("ID","EMAIL","USERNAME","NAME","SURNAME") OVERRIDING SYSTEM VALUE VALUES
 	 (1,'pippo@pippo.it','pippo','pippo','pluto');
